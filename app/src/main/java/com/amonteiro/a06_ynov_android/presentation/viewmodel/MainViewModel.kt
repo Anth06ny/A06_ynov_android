e3625914661b847ce7a7538ca54ed52830933ccf
AnthonyPCPortable <anth06ny@gmail.com>
2026-06-23 16:00:06 +0200

    Fin TP Exception

--- app/src/main/java/com/amonteiro/a06_ynov_android/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amonteiro/a06_ynov_android/presentation/viewmodel/MainViewModel.kt
@@ -9,19 +9,18 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
-
-suspend fun main(){
+suspend fun main() {
     val viewModel = MainViewModel()
-    viewModel.loadWeathers("Nice")
+    viewModel.loadWeathers("Paris")
 
-    while(viewModel.runInProgress.value) {
+    while (viewModel.runInProgress.value) {
         println("Attente....")
         delay(500)
     }
 
     //Affichage de la liste (qui doit être remplie) contenue dans la donnée observable
-    println("List : ${viewModel.dataList.value}" )
-    println("runInprogress : ${viewModel.runInProgress.value}" )
+    println("List : ${viewModel.dataList.value}")
+    println("ErrorMessage : ${viewModel.errorMessage.value}")
 
 }
 
@@ -29,14 +28,24 @@ class MainViewModel : ViewModel() {
     //MutableStateFlow est une donnée observable
     val dataList = MutableStateFlow(emptyList<Weather>())
     val runInProgress = MutableStateFlow(false)
+    val errorMessage = MutableStateFlow("")
 
 
-    fun loadWeathers(cityName:String){
+    fun loadWeathers(cityName: String) {
         runInProgress.value = true
 
         viewModelScope.launch(Dispatchers.IO) {
-            dataList.value = WeatherApiDataSource.loadWeathers(cityName)
-            runInProgress.value = false
+            try {
+                dataList.value = WeatherApiDataSource.loadWeathers(cityName)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                errorMessage.value = e.message ?: "Une erreur est survenue"
+            }
+            finally {
+                runInProgress.value = false
+            }
+
         }
+
     }
 }
