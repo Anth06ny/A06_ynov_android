55a3db5139f888b3dd95bc5e1ecdea80da4fa4b4
AnthonyPCPortable <anth06ny@gmail.com>
2026-06-25 11:43:59 +0200

    Fin TP Avant theme

--- app/src/main/java/com/amonteiro/a06_ynov_android/presentation/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a06_ynov_android/presentation/ui/screens/SearchScreen.kt
@@ -2,19 +2,28 @@ package com.amonteiro.a06_ynov_android.presentation.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.amonteiro.a06_ynov_android.R
+import com.amonteiro.a06_ynov_android.domain.model.Weather
 import com.amonteiro.a06_ynov_android.presentation.ui.theme.A06_ynov_androidTheme
 import com.amonteiro.a06_ynov_android.presentation.viewmodel.MainViewModel
 
@@ -39,20 +48,43 @@ fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = M
 
     val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
 
-    Column(modifier= modifier) {
-        Text(text = "Text1",fontSize = 20.sp)
-        Spacer(Modifier.size(8.dp))
-        Text(text = "Text2",fontSize = 14.sp,
-            modifier = Modifier.padding(10.dp)
-            )
+    Column(modifier= modifier.background(Color.LightGray).fillMaxSize()) {
 
         list.forEach {
-            PictureRowItem(it.name)
+            PictureRowItem(data = it)
         }
     }
 }
 
 @Composable
-fun PictureRowItem(text:String) {
-    Text(text = text,fontSize = 20.sp)
+fun PictureRowItem(modifier: Modifier = Modifier, data: Weather) {
+    Row(modifier = modifier.background(Color.White).fillMaxWidth()) {
+
+        //Permission Internet nécessaire
+        AsyncImage(
+            model = data.icon,
+            //Pour aller le chercher dans string.xml R de votre package com.nom.projet
+            //contentDescription = getString(R.string.picture_of_cat),
+            //En dur
+            contentDescription = "une photo de chat",
+            contentScale = ContentScale.FillWidth,
+
+            //Pour toto.png. Si besoin de choisir l'import pour la classe R, c'est celle de votre package
+            //Image d'échec de chargement qui sera utilisé par la preview
+            error = painterResource(R.drawable.error),
+            //Image d'attente.
+            //placeholder = painterResource(R.drawable.toto),
+
+            onError = { println(it) },
+            modifier = Modifier
+                .heightIn(max = 100.dp)
+                .widthIn(max = 100.dp)
+        )
+
+        Column() {
+            Text(text = data.name,fontSize = 20.sp)
+            Text(text = data.getResume().take(20) + "...",fontSize = 14.sp)
+        }
+
+    }
 }
